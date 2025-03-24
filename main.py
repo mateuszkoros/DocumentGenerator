cfb056f766639c9fe83ec0d3f710f9ca9cdef2b7
Mateusz Koroś <16102422+mateuszkoros@users.noreply.github.com>
2025-03-24 13:39:47 +0100

    Added Linux support using Libreoffice headless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import openpyxl
 import platform
 from docx import Document
@@ -6,16 +7,24 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-workbook = openpyxl.load_workbook(filename=os.getenv('VARIABLES_FILE'))
-sheet = workbook.worksheets[0]
 
 
-def get_variables():
+def check_environment_variables():
+    if (
+        'TEMPLATE_FILE' not in os.environ or
+        'VARIABLES_FILE' not in os.environ or
+        'OUTPUT_DIRECTORY' not in os.environ
+    ):
+        print('One of required environment variables is missing', file=sys.stderr)
+        exit(1)
+
+
+def get_variables(variables_sheet):
     variables_dictionary = {}
 
     # get variables list
     header_iterator = 0
-    for column in sheet.iter_cols(1, sheet.max_column):
+    for column in variables_sheet.iter_cols(1, variables_sheet.max_column):
         variables_dictionary[column[0].value] = header_iterator
         header_iterator += 1
 
@@ -23,9 +32,12 @@ def get_variables():
 
 
 if __name__ == '__main__':
+    check_environment_variables()
     os.makedirs(os.getenv('OUTPUT_DIRECTORY'), exist_ok=True)
 
-    variables = get_variables()
+    workbook = openpyxl.load_workbook(filename=os.getenv('VARIABLES_FILE'))
+    sheet = workbook.worksheets[0]
+    variables = get_variables(sheet)
 
     for row in sheet.iter_rows(min_row=2):
         # variables should always include File column
@@ -42,8 +54,9 @@ if __name__ == '__main__':
                             inline[i].text = text
         doc.save(output_docx)
         if platform.system() == 'Linux':
-            # TODO
-            print('Linux support to be done')
+            os.system(f'libreoffice --headless --convert-to pdf '
+                      f'--outdir {os.getenv('OUTPUT_DIRECTORY')}'
+                      f' {os.getenv('OUTPUT_DIRECTORY')}*.docx')
         else:
             os.system(f'rocketpdf parsedocxs {os.getenv('OUTPUT_DIRECTORY')}')
 
